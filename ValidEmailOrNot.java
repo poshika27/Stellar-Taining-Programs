a52f92ffd37636eea5bd4768ef91aa8b481127f1
Poshika <145246176+poshika27@users.noreply.github.com>
2025-03-07 12:09:42 +0530

    Create ValidEmailOrNot.java

---
+++ ValidEmailOrNot.java
@@ -0,0 +1,15 @@
+package com.steller;
+import java.util.Scanner;
+public class ValidEmailOrNot {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+			if(str.contains("com")|| str.contains("in")||str.contains("net")) {
+				System.out.println("Valid");
+			}
+			else {
+				System.out.println("Invaild");
+			}
+	}	
+}
