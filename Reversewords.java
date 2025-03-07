4380c1ef67ab6db4e29d7c182cad9a7b3794ad58
Poshika <145246176+poshika27@users.noreply.github.com>
2025-03-07 11:50:54 +0530

    Create  Reversewords.java

---
+++ Reversewords.java
@@ -0,0 +1,15 @@
+public class Reversewords{
+public static void main(String[] args){
+		Scanner s=new Scanner (System.in);
+		System.out.println("enter a string");
+		String s1=s.nextLine();
+		System.out.println(reverseWord(s1.trim()));
+	}
+	private static String reverseWord(String str) {
+		 if (!str.contains(" ")) {
+	            return str;}
+	
+		int s2=str.lastIndexOf(" ");
+		return(str.substring(s2+1))+" "+reverseWord(str.substring(0,s2));	
+	}
+}
