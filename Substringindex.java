2d18fa176b0ab8df231d433aa4463f5609e77f83
Poshika <145246176+poshika27@users.noreply.github.com>
2025-03-07 11:49:06 +0530

    Create Substringindex.java

---
+++ Substringindex.java
@@ -0,0 +1,15 @@
+public class Substringindex{
+public static void main(String[] args){
+		Scanner s=new Scanner (System.in);
+		System.out.println("enter a string");
+		String s1=s.nextLine();
+		String s2=s.nextLine();
+		if(s2.contains(s1)) {
+			System.out.println(s2.indexOf(s1));
+		}
+		else {
+			System.out.println("-1");	
+		}
+
+	}
+}
