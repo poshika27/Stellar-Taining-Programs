c976d0e6712612cbdc983226368639d1f3eac50c
Poshika <145246176+poshika27@users.noreply.github.com>
2025-03-07 12:05:10 +0530

    Update and rename RemoveVowel.java to SumOfLowerTriangle.java

---
+++ SumOfLowerTriangle.java
@@ -0,0 +1,22 @@
+package com.steller;
+import java.util.*;
+import java.util.Scanner;
+public class SumOfLowerTriangle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int matrix[][]=new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				matrix[i][j]=sc.nextInt();
+			}
+		}
+		int sum=0;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<=i;j++) {
+				sum=sum+matrix[i][j];
+			}
+		}
+		System.out.println("Sum of lower triangle: "+sum);
+	}
+}
