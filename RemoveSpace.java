c5ca19b4c3b39f74982140af47f8bebaca7e01be
Poshika <145246176+poshika27@users.noreply.github.com>
2025-03-07 12:09:12 +0530

    Create RemoveSpace.java

---
+++ RemoveSpace.java
@@ -0,0 +1,11 @@
+package com.steller;
+import java.util.Scanner;
+public class RemoveSpace {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String str1=str.replaceAll(" ","");
+		System.out.println(str1);
+	}	
+}
