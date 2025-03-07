64826934d05186349c2cf1234895ec311e7ad0af
Poshika <145246176+poshika27@users.noreply.github.com>
2025-03-07 11:36:05 +0530

    Create SumOfNaturalNumbers.java

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i; 
+        }
+        System.out.println("sum of first 100 natural no is: " + sum);
+    }
+}
